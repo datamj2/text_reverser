5f81e231b1ea54a970f74e2289311dc71aa66919
datamj2 <mjansen2@gmail.com>
2017-11-12 15:03:14 -0800

    Initial Commit

---
+++ text_reverser/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  text_reverser
+//
+//  Created by mj on 11/12/17.
+//  Copyright © 2017 mj. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ text_reverser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  text_reverser
+//
+//  Created by mj on 11/12/17.
+//  Copyright © 2017 mj. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
